a357091d78f9d45c6af542e27bfebd5821993622
Julia Testa Matos <jutemts@gmail.com>
2025-05-07 16:26:55 -0300

    atualizado

---
+++ aula012/ex001.js
@@ -0,0 +1,10 @@
+var idade = 67
+if (idade < 16){
+    console.log(` Não Vota`)
+}
+else if(idade <= 16 && idade < 18 || idade > 65){
+    console.log(`Voto opcional`)
+}
+else{
+    console.log(`Vota`)
+}
